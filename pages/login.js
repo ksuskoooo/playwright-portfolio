dd05c026ce308eb37b0e82677e686bde35a1d84a
ksuskoooo <ksuskoooo@yahoo.com>
2026-06-23 09:08:36 +0200

    feat: implement login test using Page Object Model

---
+++ pages/login.js
@@ -0,0 +1,23 @@
+export class LoginPage {
+  /** @param {import('@playwright/test').Page} page */
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = page.getByTestId('username');
+    this.passwordInput = page.getByTestId('password');
+    this.loginButton = page.getByTestId('login-button');
+  }
+
+  async goto() {
+    await this.page.goto('https://www.saucedemo.com/');
+  }
+
+  /**
+   * @param {string} username
+   * @param {string} password
+   */
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
