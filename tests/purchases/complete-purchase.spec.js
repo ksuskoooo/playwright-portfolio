6f867130ba4f709e0191d01df029cb5edca04d34
ksuskoooo <ksuskoooo@yahoo.com>
2026-07-07 12:37:36 +0200

    refactor: base test additionals and complete purcahse test with fixtures

--- tests/purchases/complete-purchase.spec.js
+++ tests/purchases/complete-purchase.spec.js
@@ -1,31 +1,31 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/login.js';
-import { InventoryPage } from '../../pages/inventory.js';
-import { CartPage } from '../../pages/cart.js';
-import { CheckoutCompletePage } from '../../pages/checkout-complete.js';
-import { CheckoutStepOnePage } from '../../pages/checkout-step-one.js';
-import { CheckOutStepTwoPage } from '../../pages/checkout-step-two.js';
+import { test, expect } from '../utils/fixtures/base-test.js';
 
-test('user can complete the purchase', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+test('user can complete the purchase', async ({
+  loginPage,
+  inventoryPage,
+  cartPage,
+  checkoutStepOnePage,
+  checkoutStepTwoPage,
+  checkoutCompletePage
+}) => {
+ 
   await loginPage.goto();
   await loginPage.login('standard_user', 'secret_sauce');
 
-  const inventoryPage = new InventoryPage(page);
+
   await inventoryPage.addJacketToCart();
   await inventoryPage.cartBadge.click();
 
-  const cartPage = new CartPage(page);
+
   await cartPage.checkoutButton.click();
 
-  const checkoutStepOnePage = new CheckoutStepOnePage(page);
   await checkoutStepOnePage.fillCheckoutForm('John', 'Johnson', '1111');
   await checkoutStepOnePage.continueButton.click()
 
-  const checkoutStepTwoPage = new CheckOutStepTwoPage(page);
+
   await checkoutStepTwoPage.finishButton.click();
 
-  const checkoutCompletePage = new CheckoutCompletePage(page);
+ 
   await expect(checkoutCompletePage.completeOrderHeader).toHaveText('Thank you for your order!');
 
 });
--- tests/utils/fixtures/base-test.js
+++ tests/utils/fixtures/base-test.js
@@ -2,6 +2,9 @@ import { test as base } from '@playwright/test';
 import { LoginPage } from '../../../pages/login.js';
 import { InventoryPage } from '../../../pages/inventory.js';
 import { CartPage } from '../../../pages/cart.js';
+import { CheckoutStepOnePage } from '../../../pages/checkout-step-one.js';
+import { CheckOutStepTwoPage} from '../../../pages/checkout-step-two.js';
+import { CheckoutCompletePage} from '../../../pages/checkout-complete.js';
 
 export const test = base.extend({
     loginPage: async ({ page }, use) => {
@@ -15,6 +18,18 @@ export const test = base.extend({
     cartPage: async ({ page }, use) => {
         const cartPage = new CartPage(page);
         await use(cartPage);
+    }, 
+    checkoutStepOnePage: async ({ page }, use) => {
+        const checkoutStepOnePage = new CheckoutStepOnePage(page);
+        await use(checkoutStepOnePage);
+    }, 
+    checkoutStepTwoPage: async ({ page }, use) => {
+        const checkoutStepTwoPage = new CheckOutStepTwoPage(page);
+        await use(checkoutStepTwoPage);
+    }, 
+    checkoutCompletePage: async ({ page }, use) => {
+        const checkoutCompletePage = new CheckoutCompletePage(page);
+        await use(checkoutCompletePage);
     }
 });
 
