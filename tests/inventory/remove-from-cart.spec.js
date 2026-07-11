b932d44bd3e27e17ccfd43c0830944137549ce37
ksuskoooo <ksuskoooo@yahoo.com>
2026-07-11 21:57:46 +0200

    refactor: remove-from-cart eith use of fixtures

--- tests/inventory/remove-from-cart.spec.js
+++ tests/inventory/remove-from-cart.spec.js
@@ -1,17 +1,20 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../pages/login.js'
-import { InventoryPage } from '../../pages/inventory.js';
 
-test('user can add jacket to the cart', async ({ page }) => {
-    const loginPage = new LoginPage(page);
+
+import { test, expect } from '../utils/fixtures/base-test';
+
+test ('user can add jacket to the cart', async ({ 
+    loginPage, 
+    inventoryPage, 
+    
+}) => 
+{ 
     await loginPage.goto();
     await loginPage.login('standard_user', 'secret_sauce');
-    
-    const inventoryPage = new InventoryPage(page);
-    await inventoryPage.addJacketToCart(); 
 
+    await inventoryPage.addJacketToCart(); 
     await inventoryPage.removeJacketFromCart();
-    
-    await expect(inventoryPage.cartBadge).toBeHidden()
+
+    await expect(inventoryPage.cartBadge).toBeHidden();
+
 
 });
