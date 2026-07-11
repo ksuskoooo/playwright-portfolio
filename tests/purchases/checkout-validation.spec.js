e960c3883f9a984fa820e2661b57cb9c4ea10618
ksuskoooo <ksuskoooo@yahoo.com>
2026-07-11 19:59:44 +0200

    refactor: checkout-validation test with a fixtures use

--- tests/purchases/checkout-validation.spec.js
+++ tests/purchases/checkout-validation.spec.js
@@ -1,27 +1,52 @@
-import { test, expect } from '@playwright/test';
-import { CartPage } from '../../pages/cart.js';
-import { LoginPage } from '../../pages/login.js';
-import { InventoryPage } from '../../pages/inventory.js';
-import { CheckoutCompletePage } from '../../pages/checkout-complete.js';
-import { CheckoutStepOnePage } from '../../pages/checkout-step-one.js';
-import { CheckOutStepTwoPage } from '../../pages/checkout-step-two.js';
+// import { test, expect } from '@playwright/test';
+// import { CartPage } from '../../pages/cart.js';
+// import { LoginPage } from '../../pages/login.js';
+// import { InventoryPage } from '../../pages/inventory.js';
+// import { CheckoutCompletePage } from '../../pages/checkout-complete.js';
+// import { CheckoutStepOnePage } from '../../pages/checkout-step-one.js';
+// import { CheckOutStepTwoPage } from '../../pages/checkout-step-two.js';
 
-test('should show error when checkout fields are empty', async ({ page }) => { 
+// test('should show error when checkout fields are empty', async ({ page }) => { 
 
-    const loginPage = new LoginPage(page);
+//     const loginPage = new LoginPage(page);
+//     await loginPage.goto();
+//     await loginPage.login('standard_user', 'secret_sauce');
+  
+//     const inventoryPage = new InventoryPage(page);
+//     await inventoryPage.addJacketToCart();
+//     await inventoryPage.cartBadge.click();
+  
+//     const cartPage = new CartPage(page);
+//     await cartPage.checkoutButton.click();
+
+//     const checkoutStepOnePage = new CheckoutStepOnePage(page);
+//     await checkoutStepOnePage.continueButton.click();
+
+//     await expect(checkoutStepOnePage.errorMessage).toHaveText('Error: First Name is required')
+
+// }) 
+
+import { test, expect } from '../utils/fixtures/base-test.js'; 
+
+test ('should show error when checkout fields are empty', async ({
+    loginPage, 
+    inventoryPage, 
+    cartPage,
+    checkoutStepOnePage, 
+}) => {
+
     await loginPage.goto();
     await loginPage.login('standard_user', 'secret_sauce');
-  
-    const inventoryPage = new InventoryPage(page);
-    await inventoryPage.addJacketToCart();
-    await inventoryPage.cartBadge.click();
-  
-    const cartPage = new CartPage(page);
+
+    await inventoryPage.addJacketToCart(); 
+    await inventoryPage.cartBadge.click(); 
+
     await cartPage.checkoutButton.click();
 
-    const checkoutStepOnePage = new CheckoutStepOnePage(page);
-    await checkoutStepOnePage.continueButton.click()
+    await checkoutStepOnePage.continueButton.click();
 
     await expect(checkoutStepOnePage.errorMessage).toHaveText('Error: First Name is required')
 
-}) 
+
+})
+
