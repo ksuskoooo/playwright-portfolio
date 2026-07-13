5eefb249dc85921979c0c5b8d9a77aa461e035cd
ksuskoooo <ksuskoooo@yahoo.com>
2026-07-13 14:32:14 +0200

    refactor: complete purchase and full checkout validation test to use faker

--- tests/purchases/full-checkout-validation.spec.js
+++ tests/purchases/full-checkout-validation.spec.js
@@ -1,86 +1,29 @@
-// import { test, expect } from '@playwright/test';
-// import { CartPage } from '../../pages/cart.js';
-// import { LoginPage } from '../../pages/login.js';
-// import { InventoryPage } from '../../pages/inventory.js';
-// import { CheckoutStepOnePage } from '../../pages/checkout-step-one.js';
-
-// const validationData = [
-//     {
-//         testName: 'should show error when FirstName is empty',
-//         firstName: '',
-//         lastName: 'Test',
-//         postalCode: '12345', 
-//         expectedError: 'Error: First Name is required',
-//     }, 
-//     {
-//         testName: 'should show error when LastName is empty',
-//         firstName: 'John', 
-//         lastName: '', 
-//         postalCode: '12345', 
-//         expectedError: 'Error: Last Name is required'
-//     },
-//     {
-//         testName: 'should show error when PostalCode is empty',
-//         firstName: 'John', 
-//         lastName: 'Doe', 
-//         postalCode: '', 
-//         expectedError: 'Error: Postal Code is required' 
-//     }
-// ];
-
-
-// for (const data of validationData) {
-    
-  
-//     test(data.testName, async ({ page }) => {
-
-//         const loginPage = new LoginPage(page); 
-//         await loginPage.goto();
-//         await loginPage.login('standard_user', 'secret_sauce');
-
-//         const inventoryPage = new InventoryPage(page);
-//         await inventoryPage.addJacketToCart();
-//         await inventoryPage.cartBadge.click();
-
-//         const cartPage = new CartPage(page); 
-//         await cartPage.checkoutButton.click();
-
-//         const checkoutStepOnePage = new CheckoutStepOnePage(page);
-//         checkoutStepOnePage.fillCheckoutForm(data.firstName, data.lastName, data.postalCode);
-//         await checkoutStepOnePage.continueButton.click();
-
-//         await expect(checkoutStepOnePage.errorMessage).toHaveText(data.expectedError);
-//     });
-// }
-
-
 import {test, expect } from '../utils/fixtures/base-test';
+import { faker } from '@faker-js/faker';    
 
 const validationData = [
     {
         testName: 'should show error when FirstName is empty',
         firstName: '',
-        lastName: 'Test',
-        postalCode: '12345', 
+        lastName: faker.person.lastName(),
+        postalCode: faker.location.zipCode('####'), 
         expectedError: 'Error: First Name is required',
     }, 
     {
         testName: 'should show error when LastName is empty',
-        firstName: 'John', 
+        firstName: faker.person.firstName(), 
         lastName: '', 
-        postalCode: '12345', 
+        postalCode: faker.location.zipCode('####'), 
         expectedError: 'Error: Last Name is required'
     },
     {
         testName: 'should show error when PostalCode is empty',
-        firstName: 'John', 
-        lastName: 'Doe', 
+        firstName: faker.person.firstName(), 
+        lastName: faker.person.lastName(), 
         postalCode: '', 
         expectedError: 'Error: Postal Code is required' 
     }
 ];
-
-
 
 for (const data of validationData) {
 
@@ -90,7 +33,7 @@ for (const data of validationData) {
     inventoryPage,
     cartPage, 
     checkoutStepOnePage, 
-    
+
     }) => 
 {
     
@@ -104,9 +47,10 @@ for (const data of validationData) {
            
                 await cartPage.checkoutButton.click();
     
-                checkoutStepOnePage.fillCheckoutForm(data.firstName, data.lastName, data.postalCode);
+                await checkoutStepOnePage.fillCheckoutForm(data.firstName, data.lastName, data.postalCode);
                 await checkoutStepOnePage.continueButton.click();
         
                 await expect(checkoutStepOnePage.errorMessage).toHaveText(data.expectedError);
             });
         }
+
